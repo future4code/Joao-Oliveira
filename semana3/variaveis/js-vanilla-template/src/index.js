dae023463d2f6ae213f19e2e138ecff5704afb29
João Oliveira <victorfnguerra@gmail.com>
2020-07-27 14:25:44 -0300

    Os exercícios foram solucionados com sucesso

---
+++ semana3/variaveis/js-vanilla-template/src/index.js
@@ -0,0 +1,117 @@
+Exercícios de Análise e Interpretação
+
+1.
+/*
+     
+        R = 10 
+            10 5 
+*/
+
+2.
+/*
+        R = 10 10 10
+*/
+
+
+//////////////////////////////////////////////////////////////
+
+Exercícios de Escrita
+
+1.
+
+a)
+//let nome
+
+b)
+//let idade
+
+c)
+//console.log(typeof nome)
+//console.log(typeof idade)
+
+d)
+/* O motivo pelo qual as variáveis a cima foram definidas como undefined é devido ao fato de as mesmas terem sido declaradas sem que tenhamos atribuídos valores as mesmas.*/
+
+e)
+
+//let nomeInput = prompt("Qual é seu nome?")
+//let idadeInput = prompt("Qual a sua idade?")
+
+//nome = nomeInput
+//idade = idadeInput
+
+//console.log(nome, idade);
+
+f)
+
+//console.log(typeof nome)
+//console.log(typeof idade)
+
+
+
+g)
+/* O fato de receberem um tipo contrário ao undefined já esperava, mas confesso que fiquei supreso quando a variável idade foi interpretada como do tipo string por mais que seja um número o valor atribuido, confesso que achei que eu havia cometido um erro ao colocar aspas no prompt de idadeInput, mas logo percebi que não.*/
+
+//console.log("Olá " + nome + ", você tem " + idade + " anos");
+
+
+2.
+a.
+
+// let name
+// let movie
+// let gender
+// let beauty
+// let favoritTec
+
+// let nameInput = prompt("Qual seu primeiro nome completo?")
+// name = nameInput
+
+// let movieInput = prompt(name + ", qual seu filme predileto?")
+// movie = movieInput
+
+// let genderInput = prompt(name + ", a qual genero você pertence?")
+// gender = genderInput
+
+// let beautyInput = prompt(name + ", de 0 a 10 qual nota de beleza você merece?")
+// beauty = beautyInput
+
+// let favoritTecInput = prompt(name + ", você prefere Grid ou flexbox? Por que?")
+// favoritTec = favoritTecInput
+
+//console.log(nameInput)
+//console.log(movieInput)
+//console.log(genderInput)
+//console.log(beautyInput)
+//console.log(favoritTecInput)
+
+3.
+
+a)
+//let ar = ["feijoada", " galinhada", "pizza", "sorvete", "coxinha"]
+// console.log(ar)
+
+b)
+// console.log("Essas são mimnhas comidas preferidas: ")
+// console.log(ar)
+
+c) DESAFIO
+// let ar = ["feijoada", secondIndex, "galinhada", "sorvete", "coxinha"]
+
+// let secondIndex = "pizza"
+
+// let favoritFood = prompt("Qual Sua Comida Preferida?")
+
+// console.log(ar)
+
+4.
+a)
+// let ar = ["Você é artista?", "Tens fome?", "Você vai hoje?"]
+
+b)
+// let resp = [" TRUE", " FALSE", " TRUE"]
+
+c)
+// console.log(ar[0] + resp[0])
+// console.log(ar[1] + resp[1])
+// console.log(ar[2] + resp[2])
