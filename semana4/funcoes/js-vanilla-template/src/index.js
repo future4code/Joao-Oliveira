52800eafc098d2598f2701af67b39e573638a97e
João Oliveira <victorfnguerra@gmail.com>
2020-08-04 15:58:45 -0300

    Exercícios Da Semana4-Aula14 Não Solucionados

---
+++ semana4/funcoes/js-vanilla-template/src/index.js
@@ -0,0 +1,221 @@
+// EXEMPLO 1 
+
+// function dizOi() {
+//     console.log("oi")
+// }
+
+// dizOi()
+
+
+// EXEMPLO 2
+
+// ------------------------------------------- EXEMPLO DE FUNCAO NOMEADA -----------------
+
+// const resultado1 = somaDoisNumeros(5, 6)
+// const resultado2 = somaDoisNumeros(10, 20)
+
+// console.log(resultado1)
+// console.log(resultado2)
+
+// function somaDoisNumeros(numero1, numero2) {
+//     const soma = numero1 + numero2
+//     return soma
+// }
+
+
+
+
+// ------------------------------------------- EXEMPLO DE FUNCAO NÃO NOMEADA -----------------
+
+
+// const somaDoisNumeros = function(numero1, numero2) {
+//     const soma = numero1 + numero2
+//     return soma
+// }
+
+// const resultado1 = somaDoisNumeros(5, 6)
+// const resultado2 = somaDoisNumeros(10, 20)
+
+// console.log(resultado1)
+// console.log(resultado2)
+
+
+
+// ------------------------------------------- EXEMPLO DE ARROW FUNCTION (FUNCAO FLECHA) -----------------
+
+
+// const somaDoisNumeros = (numero1, numero2) => {
+//     const soma = numero1 + numero2
+//     return soma
+// }
+
+
+// const resultado1 = somaDoisNumeros(5, 6)
+// const resultado2 = somaDoisNumeros(10, 20)
+
+// console.log(resultado1)
+// console.log(resultado2)
+
+
+
+
+// ------------------------------------------- EXERCÍCIO 1 -----------------
+
+// function verificaArray(arrayDeParametro) {
+//     for (let elemento of arrayDeParametro) {
+//         console.log(elemento)
+//     }
+
+//     return arrayDeParametro.length 
+// }
+
+
+// ------------------------------------------- EXERCÍCIO 2 -----------------
+
+
+// const verificaTamanhoDoArray = (arrayDeParametro) => {
+//     for (let elemento of arrayDeParametro) {
+//         console.log(elemento)
+//     }
+
+//     return arrayDeParametro.length
+// }
+
+// const profs = ["chijo", "paulinha", "sot", "caio", "severo"]
+
+// const tamanhoDoArray = verificaTamanhoDoArray(profs)
+
+
+// ---------------------------------------------- EXERCÍCIOS DA TARDE ------------
+
+
+// ---------------------------------------------- EXERCÍCIOS DE LEITURA DE CÓDIGO -----------
+
+
+//1.
+//a) A saída será 10 e 50, respectivamente.
+
+//b) Nesse caso a função irá calcular, mas não irá imprimir o resultado.
+
+//2. 
+//a) A saída será os valor dos dois primeiros índices do array especificado na function.
+
+//b)  A saída seria os nomes adicionados normalmente, Amanda e Caio. A função funcionará para qualquer array chamado. 
+
+//3.
+//a) A função que está atribuída à variável "metodo" possui um array vazio que irá receber valores desde que seja cumpridas as condições do laço for que são: Um valor que ao dividir por 2 reste zero, logo sendo - o número - par e sendo cumprida essa condição o número deve ser multiplicado por ele mesmo e por fim inserido no arrayFinal. Retornado, então, o array junto ao valor em questão  
+
+
+
+//-------------------------------------------------------- EXERCÍCIO DE ESCRITA DE CÓDIGO ----------------------
+
+//4.
+
+//a)
+
+// function sobreMim() {
+//     console.log("Eu sou João Victor, tenho 21 anos, moro em Teresina e sou estudante.")
+
+// }
+
+// sobreMim()
+
+
+//b) 
+
+// function sobreMim(nome, idade, cidade, cargo) {
+//     console.log("Eu sou " + nome + ", tenho " + idade + " anos" + ", moro em " + cidade + " e sou " + cargo)
+
+// }
+
+
+
+// const resultado = sobreMim("Joao", 21, "Teresina", true)
+
+// console.log(resultado)
+
+//5. 
+
+//a)
+
+// const number = (number1, number2) => {
+//     let soma = number1 + number2
+
+//     return soma
+// }
+
+// const resultado = number(2, 5)
+// console.log(resultado)
+
+//b) 
+
+// const tamanho = (num1, num2) => {
+//     if (num1 < num2) {
+//         console.log("O número " + num2 + " é maior")
+//     } else if (num1 === num2) {
+//         console.log("Os números são iguais")
+//     } else {
+//         console.log("O número " + num1 + " é maior")
+//     }
+
+// }
+
+// const resultado = tamanho(5, 5)
+// console.log(resultado)
+
+//c) 
+// const dezMensagens = (mensagem) => {
+//     for (t = 0; t < 10; t++) {
+//         console.log("Hello World")
+//     }
+// }
+
+
+//6. 
+
+//a)
+// const array = [10, 23, 45, 78, 90, 52, 35, 67, 84, 22]
+
+// const TamanhoDoArray = (arrayDeNumeros) => {
+//     let i = array.length
+//     console.log(i)
+// }
+// const tamanhoDoArray = TamanhoDoArray(array)
+
+//b) 
+
+// const parOuImpar = (numero) => {
+//     if (numero % 2 === 0) {
+//         console.log("É um número par")
+//     } else {
+//         console.log("É um número ímpar")
+//     }
+// }
+
+// parOuImpar(4)
+
+
+// C E D NÃO CONSEGUI
+
+///-------------------------------------- DESAFIO -------------------------
+
+//1. 
+
+// const imprimir = (parametro) => {
+//     console.log(parametro)
+
+// }
+
+// imprimir()
+
+
+// //2. 
+// const duploParametro = (parametro1, parametro2) => {
+//     const soma = parametro1 + parametro2
+
+//     return soma
+// }
+
+// const resposta = imprimir(duploParametro(5, 5))
+
+// 2. NAO CONSEGUI FAZER
