bb227b4b95f127c9f80d6df6b2ffd77f791c633b
João Oliveira <victorfnguerra@gmail.com>
2020-08-05 09:10:49 -0300

    EXERCÍCIO DA SEMANA4-AULA15

---
+++ semana4/objetos/blog/blog/blog/js/index.js
@@ -0,0 +1,26 @@
+const arrayPosts = []
+
+const infoPost - {
+    titulo: "",
+    autor: "",
+    conteudo: ""
+}
+
+function armazenarInfos() {
+    const title = document.getElementById("titulo-post").value
+    const author = document.getElementById("autor-post").value
+    const content = document.getElementById("conteudo-post").value
+
+    infopost.titulo = title
+    infopost.autor = author
+    indopost.conteudo = content
+
+}
+
+armazenarInfos()
+
+arrayPosts.push(infoPost)
+
+title.value = ""
+author.value = ""
+content.value = ""
