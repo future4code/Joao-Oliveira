aaf5d4947bc281516f0f46d2ee0ec7381850bdb1
João Oliveira <victorfnguerra@gmail.com>
2020-08-06 17:01:47 -0300

    EXERCÍCIO DA SEMANA4-AULA16

---
+++ semana4/funcoes-de-array/controlador-de-despesas/js/script.js
@@ -0,0 +1,27 @@
+document.getElementById("cadastrar").onclick = function() {
+    document.getElementById("valor").value = " "
+    document.getElementById("tipo").value = " "
+    document.getElementById("descricao").value = " "
+}
+
+const despesas = [
+    { aluguel: "" },
+    { supermercado: "" },
+    { internet: "" }
+]
+const arrayDespesas = []
+
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById("pagina-detalhes").onclick = function() {
+    location.href = "detalhes.html"
+}
